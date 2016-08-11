4b23486e13b9e0aede00c45580fe177c4c6e7dd7
hajikoma <hajikoma.works@gmail.com>
2016-08-11 21:59:14 +0900

    広告表示用クラスをリファクタリング。

--- app/src/main/java/com/hajikoma/nowalkingphone/screen/AdActivity.java
+++ app/src/main/java/com/hajikoma/nowalkingphone/screen/AdActivity.java
@@ -1,27 +1,14 @@
 package com.hajikoma.nowalkingphone.screen;
 
 import android.app.Activity;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.ad_stir.videoreward.AdstirVideoReward;
 import com.ad_stir.videoreward.AdstirVideoRewardListener;
-import com.hajikoma.nowalkingphone.Assets;
-import com.hajikoma.nowalkingphone.Score;
-import com.hajikoma.nowalkingphone.UserData;
-import com.hajikoma.nowalkingphone.framework.Game;
-import com.hajikoma.nowalkingphone.framework.Graphics;
-import com.hajikoma.nowalkingphone.framework.Graphics.PixmapFormat;
-import com.hajikoma.nowalkingphone.framework.Input.GestureEvent;
-import com.hajikoma.nowalkingphone.framework.Screen;
-import com.hajikoma.nowalkingphone.framework.Text;
-import com.hajikoma.nowalkingphone.framework.impl.AndroidGame;
-
-import java.util.List;
 
 /**
- * ゲームの結果を表示するスクリーン
+ * 広告表示アクティビティ
  */
 public class AdActivity extends Activity {
 
@@ -35,49 +22,33 @@ public class AdActivity extends Activity {
             Log.e("AD", "fail");
         }
 
-        ;
-
         public void onFailed(int spot_no) {
             Log.e("AD", "fail");
         }
-
-        ;
 
         public void onStart(int spot_no) {
             Log.e("AD", "fail");
         }
 
-        ;
-
         public void onStartFailed(int spot_no) {
             Log.e("AD", "fail");
         }
-
-        ;
 
         public void onFinished(int spot_no) {
             Log.e("AD", "fail");
         }
 
-        ;
-
         public void onReward(int spot_no) {
             isRewarded = true;
         }
-
-        ;
 
         public void onRewardCanceled(int spot_no) {
             Log.e("AD", "fail");
         }
 
-        ;
-
         public void onClose(int spot_no) {
             Log.e("AD", "fail");
         }
-
-        ;
     };
 
 
@@ -86,10 +57,6 @@ public class AdActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // サーバーサイドで成果通知を受け取る場合、メディアユーザーIDを設定します。
-        // メディアユーザーIDは、AdstirVideoReward.init()の実行前にしか機能しません。
-        // ここでは例として、メールアドレスを指定していますが、任意のIDが利用可能です。
-        AdstirVideoReward.setMediaUserID("test@gmail.com");
         // このアクティビティで使用するスポットIDについて、初期化処理を行います。
         int[] spotIds = {1};
         AdstirVideoReward.init(this, "MEDIA-989f9626", spotIds);
@@ -99,6 +66,7 @@ public class AdActivity extends Activity {
         adstirVideoReward.setAdstirVideoRewardListener(listener);
         // 広告を読み込みます。
         adstirVideoReward.load();
+        adstirVideoReward.showRewardVideo();
 
     }
 
